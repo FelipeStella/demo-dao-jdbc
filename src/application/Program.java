f6acf809d19cced7d4926e2989a46150950de893
Felipe da Silva Stella <felipe.stella@unidasul.com.br>
2020-01-15 19:58:43 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,21 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(32, "Felipe", "felipedasilva.stella@gamil.com", 
+				new Date(), 1700.00, obj);
+		
+		System.out.println(seller);
 
 	}
 
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Department implements Serializable {
 		
 	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
 	private String name;
 	
@@ -60,9 +61,7 @@ public class Department implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Department: "
-				+ "\n\nid: " + id + 
-				"\nname: " + name;
+		return "Id: " + id + ", Name: " + name;
 	}
 	
 }
