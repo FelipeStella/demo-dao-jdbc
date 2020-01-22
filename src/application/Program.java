a7bb37d2d2a8c320321ab14f3d1361d547f6ea50
Felipe da Silva Stella <felipe.stella@unidasul.com.br>
2020-01-22 17:31:42 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(32, "Felipe", "felipedasilva.stella@gamil.com", 
-				new Date(), 1700.00, obj);
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
-		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
