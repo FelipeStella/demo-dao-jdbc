8cd20180d01eea4c806476ccbb0cc1ef19752ed1
Felipe da Silva Stella <felipe.stella@unidasul.com.br>
2020-01-24 20:06:47 -0300

    implementation of the departmentDaoJDBC class and insert method

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -18,8 +20,11 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		/*Seller seller = sellerDao.findById(3);
+		System.out.println("\n\n----------------- TESTS SELLER --------------------\n\n");
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println("=== Test #1: seller findById ===");
 		
@@ -52,13 +57,23 @@ public class Program {
 		seller = sellerDao.findById(8);
 		seller.setName("Thais Duarte Oliveira");
 		sellerDao.update(seller);
-		System.out.println("Update completed");*/
+		System.out.println("Update completed");
 		
 		System.out.println("\n=== Test #6: seller delete ===");
 		System.out.print("Enter an id: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
 		System.out.println("Data successfully deleted!");
+		
+		System.out.println("\n\n----------------- TESTS DEPARTMENT --------------------\n\n");
+		
+		System.out.println("\n=== Test #1: department insert ===");
+		
+		System.out.print("Enter department name: ");
+		dep = new Department(null, sc.nextLine());
+		departmentDao.insert(dep);
+		
+		System.out.println("Date entered successfully! ID: " + dep.getId());
 
 	}
 
